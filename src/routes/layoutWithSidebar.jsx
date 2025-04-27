52db5d3f7134d28d42cc0cc182a355924c722566
sreyas <sreyaskjayan22@gmail.com>
2025-04-28 00:39:27 +0530

    implemented navbar

--- src/routes/layoutWithSidebar.jsx
+++ src/routes/layoutWithSidebar.jsx
@@ -1,3 +1,19 @@
+// import { Outlet } from "react-router-dom";
+// import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+// import { AppSidebar } from "@/components/sidebar/app-sidebar";
+
+// const LayoutWithSidebar = () => {
+//   return (
+//     <SidebarProvider>
+//       <AppSidebar className="border-r border-border" />      
+//         <SidebarTrigger className="" />
+//       <Outlet />
+//     </SidebarProvider>
+//   );
+// };
+
+// export default LayoutWithSidebar;
+
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
@@ -5,11 +21,17 @@ import { AppSidebar } from "@/components/sidebar/app-sidebar";
 const LayoutWithSidebar = () => {
   return (
     <SidebarProvider>
-      <AppSidebar className="border-r border-border" />
-      <div className="flex flex-col">
-        <SidebarTrigger className="absolute pt-10 ps-5" />
+      <div className="flex w-screen">
+        <div className="border-r border-border">
+          <AppSidebar />
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="h-12 border-b border-border flex items-center px-4">
+            <SidebarTrigger />
+          </div>
+            <Outlet />          
+        </div>
       </div>
-      <Outlet />
     </SidebarProvider>
   );
 };
